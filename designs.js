035d0098f212bd898be79b6d6904d50382101201
Matthew McCane <MatthewLeeMcCane@gmail.com>
2022-05-10 19:18:59 -0400

    feat: Add variables to designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,20 @@
 // Select color input
+const color_input = document.getElementById("colorPicker")
+// Select canvas input
+const canvas_input = document.getElementById("pixelCanvas")
+// Select height input
+const height_input = document.getElementById("inputHeight")
+// Select width
+const width_input = document.getElementById("inputWidth")
 // Select size input
+const size_input = document.getElementById("sizePicker")
+
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
 
-// Your code goes here!
+
+
 
 }
