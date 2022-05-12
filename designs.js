82befc799c9af39110dcb69988c02de6e50ea9fc
Matthew McCane <MatthewLeeMcCane@gmail.com>
2022-05-12 16:43:14 -0400

    event listeners

--- designs.js
+++ designs.js
@@ -1,29 +1,26 @@
 // Select color input
-const colorInput = document.getElementById("colorPicker");// Select size input
+const colorInput = document.getElementById("colorPicker");
+// Select size input
 const sizeInput = document.getElementById("sizePicker");
 // Table input
 const tableInput = document.getElementById("pixelCanvas");
 // Input for height value
-const heightInput = document.getElementById("inputHeight").value;
+const heightInput = document.getElementById("inputHeight");
 // Input for width value
-const widthInput = document.getElementById("inputWidth").value;
+const widthInput = document.getElementById("inputWidth");
 
 
 // Listens for an event on the submit button.
 sizeInput.addEventListener("submit", function (event) {
     event.preventDefault()
     // Logic to add the grid.
-    makeGrid()
-})
+    makeGrid(heightInput, widthInput)
+});
 
-tableInput.addEventListener("click", function (event) {
-    if (event.target.nodeName === 'TD') {
 
-    }
-})
 
 // When size is submitted by the user, call makeGrid()
 
-// function makeGrid() {
-
-//     }
+function makeGrid(heightInput, widthInput) {
+    tableInput.innerHTML = "";
+}
